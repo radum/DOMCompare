4a5b5f6e8adb8de6e0ed60c0e9b44be29498c8b2
radum <radu.micu@gmail.com>
2025-08-07 21:27:06 +0100

    Refactor UI and integrate pretty print functionality for HTML sources

--- components/diff2html.ts
+++ components/diff2html.ts
@@ -1,10 +1,15 @@
+import type { GlobalConfiguration } from '@dprint/formatter';
 import type { Diff2HtmlUIConfig } from 'diff2html/lib/ui/js/diff2html-ui-slim.js';
+import { createStreaming } from '@dprint/formatter';
 import { storage } from '@wxt-dev/storage';
 import { createTwoFilesPatch } from 'diff';
 import { defaultDiff2HtmlUIConfig, Diff2HtmlUI } from 'diff2html/lib/ui/js/diff2html-ui-slim.js';
 import { browser } from 'wxt/browser';
-import { DOCTYPE_KEY_PREFIX, RENDERED_OBJURL_KEY_PREFIX, UA_KEY_PREFIX } from '@/utils/constants';
+import { RENDERED_OBJURL_KEY_PREFIX } from '@/utils/constants';
 // import * as Diff2Html from 'diff2html';
+
+let renderedSourceHTML: string;
+let rawSourceHTML: string;
 
 function drawDiff2HTML(diffString: string, config: Diff2HtmlUIConfig, elements: any): void {
 	const diff2htmlUi = new Diff2HtmlUI(elements.structure.diffTarget, diffString, config);
@@ -16,7 +21,38 @@ function drawDiff2HTML(diffString: string, config: Diff2HtmlUIConfig, elements: 
 	// document.getElementById('diff-container').innerHTML = diffHTML;
 }
 
-function loadDiff2HTML(diffString: string) {
+let htmlFormatter: Awaited<ReturnType<typeof createStreaming>> | null = null;
+async function getHtmlFormatter() {
+	if (htmlFormatter) {
+		return htmlFormatter;
+	}
+	const globalConfig: GlobalConfiguration = {
+		lineWidth: 160
+	};
+	htmlFormatter = await createStreaming(
+		fetch('https://plugins.dprint.dev/g-plane/markup_fmt-v0.23.1.wasm')
+	);
+
+	htmlFormatter.setConfig(globalConfig, {
+		useTabs: true,
+		quotes: 'double',
+		formatComments: false,
+		scriptIndent: false,
+		styleIndent: false,
+		closingBracketSameLine: false,
+		closingTagLineBreakForEmpty: 'fit',
+		preferAttrsSingleLine: false,
+		singleAttrSameLine: true,
+		whitespaceSensitivity: 'css',
+		doctypeKeywordCase: 'upper'
+	});
+	return htmlFormatter;
+}
+
+function loadDiff2HTML(diff: string) {
+	// This is assigned again as it could change if pretty printing is applied
+	let diffString = diff;
+
 	const diff2HTMLConfig: Diff2HtmlUIConfig = {
 		...defaultDiff2HtmlUIConfig,
 		drawFileList: false,
@@ -35,6 +71,7 @@ function loadDiff2HTML(diffString: string) {
 
 	const btnLayoutSideBySide = document.getElementById('btn__layout-side-by-side');
 	const btnLayoutStacked = document.getElementById('btn__layout-stacked');
+	const btnPrettyPrint = document.getElementById('btn__pretty-print');
 
 	btnLayoutSideBySide!.addEventListener('click', () => {
 		diff2HTMLConfig.outputFormat = 'side-by-side';
@@ -48,6 +85,32 @@ function loadDiff2HTML(diffString: string) {
 		drawDiff2HTML(diffString, diff2HTMLConfig, elements);
 		btnLayoutSideBySide!.classList.remove('is-active');
 		btnLayoutStacked!.classList.add('is-active');
+	});
+
+	btnPrettyPrint!.addEventListener('click', async () => {
+		const formatter = await getHtmlFormatter();
+		const isPretty = btnPrettyPrint!.classList.contains('is-active');
+
+		let newRawSource = rawSourceHTML;
+		let newRenderedSource = renderedSourceHTML;
+
+		if (!isPretty) {
+			newRawSource = formatter.formatText({
+				filePath: 'raw.html',
+				fileText: rawSourceHTML
+			});
+			newRenderedSource = formatter.formatText({
+				filePath: 'rendered.html',
+				fileText: renderedSourceHTML
+			});
+			btnPrettyPrint!.classList.add('is-active');
+		}
+		else {
+			btnPrettyPrint!.classList.remove('is-active');
+		}
+
+		diffString = createTwoFilesPatch('raw.html', 'rendered.html', newRawSource, newRenderedSource);
+		drawDiff2HTML(diffString, diff2HTMLConfig, elements);
 	});
 }
 
@@ -102,6 +165,7 @@ function initToolBarControls() {
 	});
 }
 
+// MAIN FUNCTION
 export async function initDiff2HTML() {
 	// get tabID from URL parameter
 	const thisURL = new URL(window.location.href);
@@ -119,8 +183,8 @@ export async function initDiff2HTML() {
 	}
 
 	browser.tabs.get(Number.parseInt(tabID, 10), async (tab) => {
-		const renderedSourceHTML = await fetchHTMLSource(renderedSourceURL);
-		const rawSourceHTML = await fetchHTMLSource(tab.url as string);
+		renderedSourceHTML = await fetchHTMLSource(renderedSourceURL);
+		rawSourceHTML = await fetchHTMLSource(tab.url as string);
 
 		document.getElementById('analyzing-status-element')!.textContent = tab.url || 'Unknown URL';
 
@@ -131,6 +195,12 @@ export async function initDiff2HTML() {
 	});
 }
 
+/**
+ * Updates the UI sections with rendered source containers with the provided HTML.
+ *
+ * @param raw - The raw source string to display in the 'original-source-container' element.
+ * @param rendered - The rendered source string to display in the 'rendered-source-container' element.
+ */
 function addSourcesToUI(raw: string, rendered: string) {
 	const originalContainer = document.getElementById('original-source-container');
 
@@ -145,6 +215,16 @@ function addSourcesToUI(raw: string, rendered: string) {
 	}
 }
 
+/**
+ * Fetches the HTML source from the specified URL.
+ *
+ * Makes an HTTP GET request to the provided URL and returns the response body as a string.
+ * Throws an error if the response status is not OK (status code outside the 200-299 range).
+ *
+ * @param url - The URL from which to fetch the HTML source.
+ * @returns A promise that resolves to the HTML source as a string.
+ * @throws {Error} If the HTTP response status is not OK.
+ */
 async function fetchHTMLSource(url: string) {
 	return fetch(url).then((response) => {
 		if (!response.ok) {
